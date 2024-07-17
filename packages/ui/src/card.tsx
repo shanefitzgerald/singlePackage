443a0f72ae76419713cc9884cd41e37060a6d4b7
shanefitzgerald <shane.fitzgerald@mantelgroup.com.au>
2024-07-17 10:20:32 +1000

    fixes bug in card compoent (#6)

--- packages/ui/src/card.tsx
+++ packages/ui/src/card.tsx
@@ -19,7 +19,7 @@ export function Card({
       target="_blank"
     >
       <h2>
-        {title} <span>-&gt; v:{version}</span>
+        {title} <span>-&gt; v:{version} fixes bug in card</span>
       </h2>
       <p>{children}</p>
     </a>
